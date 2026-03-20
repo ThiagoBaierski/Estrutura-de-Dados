a586a0f902b08a1d493f111070e452cfca98c215
gustavoboeing12 <gustavofratonib@gmail.com>
2026-03-20 20:16:00 -0300

    AULA DIA 20-03-2026

--- Estrutura de Dados/src/Vetor.java
+++ Estrutura de Dados/src/Vetor.java
@@ -3,6 +3,7 @@ public class Vetor {
     private int tamanho;
     private int numElementos = 0;
 
+
     public Vetor(int tamanho){
          this.tamanho = tamanho;
          vetor = new int[tamanho];
@@ -17,11 +18,8 @@ public class Vetor {
         numElementos++;
         return true;
     }
-
-    public boolean remover(int valor){
-        return false;
-    }
     public int buscar(int valor){
+
         for(int i = 0; i < numElementos; i++){
             if(vetor[i] == valor){
                 return i;
@@ -29,6 +27,43 @@ public class Vetor {
         }
         return -1;
     }
+
+    public int buscaBinaria(int valor){
+
+        int inicio = 0;
+        int fim = numElementos;
+        int chute =(fim-inicio)/2;
+
+        while(inicio < fim){
+
+            if (vetor[chute] == valor) {
+                return chute;
+            }
+            if(valor>vetor[chute]){
+                inicio = chute;
+                chute =(fim+inicio)/2;
+            }
+            else{
+                fim = chute;
+                chute =(fim-inicio)/2;
+            }
+        }
+    return -1;
+    }
+    public boolean remover(int valor){
+        int posicao = buscar(valor);
+        if (posicao !=1){
+            for(int i = posicao; i < numElementos-1;i++){
+                vetor[i] = vetor[i+1];
+            }
+            vetor[numElementos-1] = 0;
+            numElementos--;
+            return true;
+        }
+        return false;
+    }
+
+
     public int getNumElementos(){
         return numElementos;
     }

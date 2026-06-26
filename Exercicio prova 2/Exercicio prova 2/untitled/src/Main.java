27ed6e4ed57e0caf14185d30221d0b9681111515
Thiago Baierski <124676533+ThiagoBaierski@users.noreply.github.com>
2026-06-26 19:10:46 -0300

    Add files via upload

---
+++ Exercicio prova 2/Exercicio prova 2/untitled/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+      ArvoreBinaria arvore = new ArvoreBinaria();
+      arvore.inserir(10);
+      arvore.inserir(5);
+      arvore.inserir(3);
+      arvore.inserir(23);
+      arvore.inserir(14);
+
+
+    }
+}
